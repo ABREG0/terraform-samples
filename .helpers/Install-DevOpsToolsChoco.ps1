fdaf6b28b1dc94102328e244fd5ee75ff8a56b15
elsalvos <17753252+elsalvos@users.noreply.github.com>
2021-08-26 16:32:01 -0700

    Update Install-DevOpsToolsChoco.ps1

--- .helpers/Install-DevOpsToolsChoco.ps1
+++ .helpers/Install-DevOpsToolsChoco.ps1
@@ -27,14 +27,12 @@ function Install-choco {
 }
 
 function Install-azModule {
-    $azModule  = $(cmd /c "C:\Program Files\PowerShell\7\pwsh.exe" -c {Get-InstalledModule -Name az -ErrorAction SilentlyContinue | Select-Object version})
+    $azModule  = $(cmd /c "C:\Program Files\PowerShell\7\pwsh.exe" -WorkingDirectory ~ -c {Get-InstalledModule -Name az -ErrorAction SilentlyContinue | Select-Object version})
 
     if($null -eq $azModule){
 
     write-host " az Module is NOT Installed... Installing Azure az module" -ForegroundColor red 
-    start-process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -ArgumentList '-c "& {Install-Module -Name Az -Repository PSGallery -Force -AllowClobber}"'
-    #Install-Module -Name Az -Repository PSGallery -Force -AllowClobber
-
+    start-process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -WorkingDirectory ~  -ArgumentList '-c "& {Install-Module -Name Az -Repository PSGallery -Force -AllowClobber}"'
     }
      else{
 
@@ -42,9 +40,80 @@ function Install-azModule {
      }
 }
 
+
+$vcExtentions = @'
+4ops.terraform
+amazonwebservices.aws-toolkit-vscode
+azps-tools.azps-tools
+bencoleman.armview
+bierner.markdown-yaml-preamble
+DavidAnson.vscode-markdownlint
+donjayamanne.python-extension-pack
+ed-elliott.azure-arm-template-helper
+erd0s.terraform-autocomplete
+garytyler.darcula-pycharm
+GitHub.vscode-pull-request-github
+golang.go
+GrapeCity.gc-excelviewer
+hashicorp.terraform
+KnisterPeter.vscode-github
+magicstack.MagicPython
+mark-tucker.aws-cli-configure
+mechatroner.rainbow-csv
+mindaro-dev.file-downloader
+mindginative.terraform-snippets
+ms-azure-devops.azure-pipelines
+ms-azuretools.vscode-azureappservice
+ms-azuretools.vscode-azurefunctions
+ms-azuretools.vscode-azureresourcegroups
+ms-azuretools.vscode-azurestorage
+ms-azuretools.vscode-azureterraform
+ms-azuretools.vscode-azurevirtualmachines
+ms-azuretools.vscode-bicep
+ms-azuretools.vscode-cosmosdb
+ms-azuretools.vscode-docker
+ms-dotnettools.vscode-dotnet-runtime
+ms-python.python
+ms-python.vscode-pylance
+ms-toolsai.jupyter
+ms-vscode-remote.remote-containers
+ms-vscode-remote.remote-ssh
+ms-vscode-remote.remote-ssh-edit
+ms-vscode-remote.remote-wsl
+ms-vscode-remote.vscode-remote-extensionpack
+ms-vscode.azure-account
+ms-vscode.azurecli
+ms-vscode.notepadplusplus-keybindings
+ms-vscode.powershell
+ms-vscode.vscode-node-azure-pack
+msazurermtools.azurerm-vscode-tools
+redhat.vscode-commons
+redhat.vscode-yaml
+rosshamish.kuskus-kusto-syntax-highlighting
+run-at-scale.terraform-doc-snippets
+tht13.python
+TomAustin.azure-devops-yaml-pipeline-validator
+VisualStudioExptTeam.vscodeintellicode
+yzhang.markdown-all-in-one
+'@
+function Add-vsCodeExtentions {
+    param($vcExtentions)
+  $vcExtentions = $vcExtentions -replace "`n", ''
+  $vcExtentions = $vcExtentions.Split("`r")
+
+  $vcExtentions.Count
+
+  for ($i = 0; $i -lt $vcExtentions.Count; $i++) {
+      $ext = "$($vcExtentions[$i])"
+      Write-Host "Installing extention Name: $($vcExtentions[$i])"
+      & cmd /c code --install-extension $ext --force
+  }
+}
+
+
 Install-choco
 
-choco install powershell-core --version=7.1.1 -y --force --force-dependencies
+choco install powershell-core --version=7.1.4 -y --force --force-dependencies
     
 choco install azure-cli -y --force --force-dependencies
 
@@ -53,5 +122,9 @@ choco install vscode -y --force --force-dependencies
 choco install git -y --force --force-dependencies
 
 choco install terraform -y --force --force-dependencies
+
+& refreshenv
+
+Add-vsCodeExtentions -vcExtentions $vcExtentions
 
 Install-azModule
