9ebf05cc64d583a176e4ad3f3e3cf546619b27b2
el <el@exam.com>
2024-08-22 19:35:10 -0700

    final alz orchestrator mr v2

--- testing/alz-em-v2/main.tf
+++ testing/alz-em-v2/main.tf
@@ -5,14 +5,14 @@
 # }
 
 # This is required for resource modules
-resource "azurerm_resource_group" "this_my" {
+resource "azurerm_resource_group" "this" {
     for_each = {for kk, kv in local.resource_groups : kk => kv
         } # {for kk, kv in local.resource_groups.RGs : kk => kv}
   location = each.value.location
   name     = each.value.resource_group_name
 }
 # This is required for resource modules
-resource "azurerm_resource_group" "this_my2" {
+resource "azurerm_resource_group" "this2" {
     for_each = {for kk, kv in local.resource_groups2.RGs : kk => kv
         } # {for kk, kv in local.resource_groups.RGs : kk => kv}
   location = each.value.location
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "this_my2" {
 
     #Defining the first virtual network (vnet-1) with its subnets and settings.
     module "vnet1" {
-        depends_on = [ azurerm_resource_group.this_my, azurerm_network_security_group.this, azurerm_route_table.this, ]
+        depends_on = [ azurerm_resource_group.this, ]
         for_each = {for kk, kv in local.creating_nested_objects_vnets2 : kv.name => kv
         } #local.creating_nested_objects_vnets2 # {for kk, kv in local.creating_nested_objects_vnets2 : kk => kv }
         source              = "Azure/avm-res-network-virtualnetwork/azurerm"
@@ -112,7 +112,7 @@ resource "azurerm_resource_group" "this_my2" {
 
     #Creating a Route Table with a unique name in the specified location.
     resource "azurerm_route_table" "this" {
-        depends_on = [ azurerm_resource_group.this_my,  ]
+        depends_on = [ module.vnet1, module.vnet1,  ]
         for_each = {for kk, kv in local.creating_nested_objects_rt2 : kv.name => kv
         }
         location            = each.value.location
@@ -120,8 +120,23 @@ resource "azurerm_resource_group" "this_my2" {
         resource_group_name = each.value.resource_group_name
     }
 
+    resource "azurerm_subnet_route_table_association" "this" {
+        depends_on = [ azurerm_route_table.this  ]
+        for_each = { for top_key, top_value in flatten([
+                for net_key, net_v in local.creating_nested_objects_vnets2 : [
+                    for snet_k, snet_v in net_v.subnets : {
+                        snet_id = module.vnet1[net_v.name].subnets[snet_v.name].resource_id
+                        rt_id = azurerm_route_table.this[snet_v.rt_key].id
+                    } if snet_v.rt_key != null || snet_v.nsg_key != null
+                ]
+              ]) : "${top_key}" => top_value
+    }
+      subnet_id = each.value.snet_id
+      route_table_id = each.value.rt_id
+    }
+
     resource "azurerm_network_security_group" "this" {
-        depends_on = [ azurerm_resource_group.this_my,  ]
+        depends_on = [ azurerm_subnet_route_table_association.this  ]
         for_each = {for kk, kv in local.creating_nested_objects_nsg2 : kv.name => kv
         }
         location            = each.value.location
@@ -139,6 +154,21 @@ resource "azurerm_resource_group" "this_my2" {
             source_address_prefix      =  "10.2.3.1" # jsondecode(data.http.public_ip.response_body).ip
             source_port_range          = "*"
         }
+    }
+    resource "azurerm_subnet_network_security_group_association" "name" {
+        depends_on = [ azurerm_network_security_group.this ]
+        for_each = { for top_key, top_value in flatten([
+                for net_key, net_v in local.creating_nested_objects_vnets2 : [
+                    for snet_k, snet_v in net_v.subnets : {
+                        snet_id = module.vnet1[net_v.name].subnets[snet_v.name].resource_id
+                        nsg_id = azurerm_network_security_group.this[snet_v.nsg_key].id
+                    } if snet_v.rt_key != null || snet_v.nsg_key != null
+                ]
+              ]) : "${top_key}" => top_value
+    }
+      subnet_id = each.value.snet_id
+      network_security_group_id = each.value.nsg_id
+      
     }
 
     ## Section to provide a random Azure region for the resource group
--- testing/alz-em-v2/outputs.tf
+++ testing/alz-em-v2/outputs.tf
@@ -1,8 +1,26 @@
-output "nsg_by_id" {
-  value = azurerm_network_security_group.this["nsg-fw_ew_trust-test-wus2-001"].id
-}
-output "vnet_module" {
-  value = module.vnet1["ohemr-vnet-hub_fw-shared-wus2-002"].subnets["GatewaySubnet"].resource_id
+# output "nsg_by_id" {
+#   value = azurerm_network_security_group.this 
+# #   azurerm_network_security_group.this["nsg-fw_ew_trust-test-wus2-001"].id
+# }
+output "associate" {
+  value = { for top_key, top_value in flatten([
+                for net_key, net_v in local.creating_nested_objects_vnets2 : [
+                    for snet_k, snet_v in net_v.subnets : {
+                        vnet = net_v.name
+                        # "${snet_v.rt_key}"
+                        rt = snet_v.rt_key
+                        # "${snet_v.nsg_key}"        
+                        nsg = snet_v.nsg_key
+                        # "${snet_v.name}" 
+                        snet = snet_v.name
+                        snet_id = module.vnet1[net_v.name].subnets[snet_v.name].resource_id
+                        nsg_id = azurerm_network_security_group.this[snet_v.nsg_key].id
+                        rt_id = azurerm_route_table.this[snet_v.rt_key].id
+                    } if snet_v.rt_key != null || snet_v.nsg_key != null
+                ]
+              ]) : "${top_key}" => top_value
+    }
+  #module.vnet1["ohemr-vnet-hub_fw-shared-wus2-002"].subnets["GatewaySubnet"].resource_id
 }
 # # output "rt_id" {
 # #   value = local.rt_id
