2b42ea24d23c1cebce27cfaf434e8ea957549e8f
el <el@exam.com>
2024-08-22 19:37:04 -0700

    final alz orchestrator mr v2

--- testing/alz-em-v2/variables.tf
+++ testing/alz-em-v2/variables.tf
@@ -63,63 +63,63 @@ variable "hub_connection" {
             virtual_networks = {
                 ohemr-vnet-hub_fw-shared-wus2-002 = {
                     name = "ohemr-vnet-hub_fw-shared-wus2-002"
-                    virtual_network_address_space = ["10.150.192.0/23", "10.150.194.0/25"]
+                    virtual_network_address_space = ["10.50.192.0/23", "10.50.194.0/25"]
                     subnets = {
                         "GatewaySubnet" = {
                         name = "GatewaySubnet"
-                        address_prefixes = ["10.150.193.0/24",]
+                        address_prefixes = ["10.50.193.0/24",]
                         }
                         "snet-fw_ew_trust-shared-wus2-001" = {
                             name             = "snet-fw_ew_trust-shared-wus2-001"
-                            address_prefixes = ["10.150.192.0/26",]
+                            address_prefixes = ["10.50.192.0/26",]
                             nsg_key = "nsg-fw_ew_trust-shared-wus2-001"
                             rt_key = "rt-fw_ew_trust-shared-wus2-001"
                         }
                         "snet-fw_ew_trust-test-wus2-001" = {
                             name             = "snet-fw_ew_trust-test-wus2-001"
-                            address_prefixes = ["10.150.192.64/26",]
+                            address_prefixes = ["10.50.192.64/26",]
                             nsg_key = "nsg-fw_ew_trust-test-wus2-001"
                             rt_key = "rt-fw_ew_trust-test-wus2-001"
                         }
                         # {
                         #     name             = "fw_ew_trust-backhaul-wus2-001"
-                        #     address_prefixes = ["10.150.192.128/26",]
+                        #     address_prefixes = ["10.50.192.128/26",]
                         # },
                         # # {
                         # #   name             = "fw_ew_mgmt-shared-wus2-001 "
-                        # #   address_prefixes = ["10.150.192.192/27",]
+                        # #   address_prefixes = ["10.50.192.192/27",]
                         # # },
                         # {
                         #     name             = "rt-fw_ew_mgmt-shared-wus2-001"
-                        #     address_prefixes = ["10.150.192.224/27",]
+                        #     address_prefixes = ["10.50.192.224/27",]
                         # },
                         # {
                         #     name             = "fw_ingress_untrust-shared-wus2-001"
-                        #     address_prefixes = ["10.150.193.0/26",]
+                        #     address_prefixes = ["10.50.193.0/26",]
                         # },
                         # {
                         #     name             = "fw_ingress_trust-shared-wus2-001"
-                        #     address_prefixes = ["10.150.193.64/26",]
+                        #     address_prefixes = ["10.50.193.64/26",]
                         # },
                         # {
                         #     name             = "fw_ingress_trust-test-wus2-001"
-                        #     address_prefixes = ["10.150.193.128/26",]
+                        #     address_prefixes = ["10.50.193.128/26",]
                         # },
                         # {
                         #     name             = "fw_ingress_mgmt-shared-wus2-001"
-                        #     address_prefixes = ["10.150.193.192/27",]
+                        #     address_prefixes = ["10.50.193.192/27",]
                         # },
                         # {
                         #     name             = "fw_pe-shared-wus2-001"
-                        #     address_prefixes = ["10.150.193.224/27",]
+                        #     address_prefixes = ["10.50.193.224/27",]
                         # },
                         # {
                         #     name             = "inbound_pvtrsvlr-shared-wus2-001"
-                        #     address_prefixes = ["10.150.194.0/26",]
+                        #     address_prefixes = ["10.50.194.0/26",]
                         # },
                         # # {
                         # #   name             = "outbound_pvtrsvlr-shared-wus2-001"
-                        # #   address_prefixes = ["10.150.143.64/26",]
+                        # #   address_prefixes = ["10.50.143.64/26",]
                         # # }
 
                     }
